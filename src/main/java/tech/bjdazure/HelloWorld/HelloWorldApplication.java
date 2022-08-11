381c81fb63ebc8bb89f665548f2ae9ee4d350e99
Brian <brian@briandenicola.com>
2022-08-11 14:48:31 +0000

    Baby steps

--- src/main/java/tech/bjdazure/HelloWorld/HelloWorldApplication.java
+++ src/main/java/tech/bjdazure/HelloWorld/HelloWorldApplication.java
@@ -1,7 +1,14 @@
 package tech.bjdazure.HelloWorld;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.UUID;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -10,4 +17,48 @@ public class HelloWorldApplication {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
 
+}
+
+@RestController
+class RestApiDemoController {
+	private List<Coffee> coffees = new ArrayList<>();
+
+	public RestApiDemoController() {
+		coffees.addAll(List.of(
+			new Coffee("Americano"),
+			new Coffee("Cappuccino"),
+			new Coffee("Latte")
+		));
+	}
+
+	@RequestMapping(value = "/coffees", method = RequestMethod.GET)
+	Iterable<Coffee> getCoffees() {
+		return coffees; 
+	}
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 }
