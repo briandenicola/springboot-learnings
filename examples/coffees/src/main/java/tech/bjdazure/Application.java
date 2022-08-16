f29bab3f4bfbc3ebeb00816d5653add101f6affa
Brian <brian@briandenicola.com>
2022-08-16 17:50:50 +0000

    Turned off banner

--- examples/coffees/src/main/java/tech/bjdazure/Application.java
+++ examples/coffees/src/main/java/tech/bjdazure/Application.java
@@ -1,6 +1,7 @@
 package tech.bjdazure;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,6 +12,9 @@ public class Application {
 	private static final Class<Application> applicationClass = Application.class;
 
 	public static void main(String[] args) {
+		
+		SpringApplication application = new SpringApplication(applicationClass);
+		application.setBannerMode(Banner.Mode.OFF);
 		SpringApplication.run(applicationClass, args);
 	}
 }
